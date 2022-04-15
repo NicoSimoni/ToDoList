98b78030b77e1c1d01c23bab083cde5fdbaca62c
Nicolas Simoni <100721031+NicoSimoni@users.noreply.github.com>
2022-04-15 20:09:10 -0300

    gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import {App} from './app.jsx'
-
+//Libreria Bootswatch
 import 'bootswatch/dist/superhero/bootstrap.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
